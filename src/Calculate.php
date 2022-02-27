6d5e0cd81d432bfda812b2e00589fa1323c92772
Civan Yavuzsen <civanyavuzsen@gmail.com>
2022-02-27 15:40:50 +0100

    chore: add template for calculate service

---
+++ src/Calculate.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Drupal\payday;
+
+/**
+ * Service description.
+ */
+class Calculate {
+
+  /**
+   * Method description.
+   */
+  public function doSomething() {
+    // @DCG place your code here.
+  }
+
+}
